82d13585dea0c9e96254011a7f5caa28205acd24
Christopher D Koon <CHRKOON@uat.edu>
2018-07-12 18:23:40 -0700

    Add files via upload

---
+++ CSC102 Assignment 1.py
@@ -0,0 +1,7 @@
+#Write a computer program that prints the following information to the screen:
+#Name
+#Say one cool/exciting thing you have done.
+#What are you interested in (hobbies, etc...)?
+#What you aspire to become (professions, long-term goals, etc...)?
+#Inputs and Outputs
+print("Christopher Koon \nI was enlisted in the United States Air Force. \nI like to play games, learn new things in IT and experiement with light & magnetism. \nI want to invent something using light and spend my free time creating the way people interact with my own virtual world.")
